111eaab17efe132157b59f8027f8113979dd9082
pdp <john.lgd65@gmail.com>
2023-04-28 23:14:58 +0500

    initial

--- src/main/java/uz/pdp/springboot/springboot/post/PostRepository.java
+++ src/main/java/uz/pdp/springboot/springboot/post/PostRepository.java
@@ -1,16 +1,8 @@
 package uz.pdp.springboot.springboot.post;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PostRepository {
-    final JdbcTemplate jdbcTemplate;
-
-    public PostRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-
+public interface PostRepository extends JpaRepository<Post,Integer> {
 }
